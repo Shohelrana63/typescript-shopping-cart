7c6b481e3e86816452aa353cbd4dfef62bd374e9
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2021-08-25 21:36:20 +0600

    products data displayed in console

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+// Components
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+// Styles
+import { Wrapper } from './App.styles';
+//types
+export type CartItemType = {
+  id: number,
+  category: string,
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products/')).json();
 
-function App() {
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products',
+    getProducts
+  )
+  console.log('data', data);
   return (
     <div className="App">
       start
