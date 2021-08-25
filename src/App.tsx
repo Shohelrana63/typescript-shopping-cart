9a9ddc5afc127c5bb4535c69f49cc88ad4a536bb
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2021-08-25 22:45:49 +0600

    products properly show in UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { useState } from 'react';
 import { useQuery } from 'react-query';
 // Components
 import Drawer from '@material-ui/core/Drawer';
-import LinearProgress from '@material-ui/core/LinearProgress';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
+import Item from './Item/Item';
 // Styles
 import { Wrapper } from './App.styles';
 //types
@@ -27,9 +28,27 @@ const App = () => {
     getProducts
   )
   console.log('data', data);
+  const getTotalItems = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading)
+    return <div style={{ textAlign: 'center', marginTop: '300px' }}>
+      <CircularProgress color="secondary" />
+    </div>
+  if (error) return <div>Something Went Wrong ...!</div>
   return (
-    <div className="App">
-      start
+    <div style={{ background: "#1d1d1d" }}>
+      <Wrapper>
+        <Grid container spacing={3} >
+          {data?.map(item => (
+            <Grid item key={item.id} xs={12} sm={4}>
+              <Item item={item} handleAddToCart={handleAddToCart} />
+            </Grid>
+          ))}
+        </Grid>
+      </Wrapper>
     </div>
   );
 }
